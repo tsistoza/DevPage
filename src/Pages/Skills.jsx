b89825c72158723f83892cce57c896beeb1bda41
Vinton Sistoza <tsistoza@gmail.com>
2025-08-31 10:52:41 -1000

    one section skills page done

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -1,8 +1,9 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
 import { ListGroup } from 'react-bootstrap';
-import { GearFill, PlusSquareDotted } from 'react-bootstrap-icons';
+import { GearFill, Plus, PlusSquareDotted } from 'react-bootstrap-icons';
 
 const Skills = () => {
     return (
@@ -22,19 +23,58 @@ const Skills = () => {
                 </Row>
 
                 <Row className="d-flex p-5 m-0">
-                    <Col>
-                        <h3 className="fw-semibold">Programming Languages</h3>
+
+                    <Col className="d-flex border-end pe-5 align-items-center justify-content-end">
+                        <h3 className="fw-semibold text-end">Programming Languages</h3>
                     </Col>
-                    <Col>
-                        <ListGroup>
-                            <ListGroup.Item>C++</ListGroup.Item>
-                            <ListGroup.Item>C</ListGroup.Item>
-                            <ListGroup.Item>Python</ListGroup.Item>
-                            <ListGroup.Item>Assembly x86/MIPS</ListGroup.Item>
-                            <ListGroup.Item>C#</ListGroup.Item>
-                            <ListGroup.Item>Javascript</ListGroup.Item>
-                            <ListGroup.Item>SQL</ListGroup.Item>
-                        </ListGroup>
+
+                    <Col className="ps-5">
+                        <Table striped variant='dark'>
+                            <thead>
+                                <tr>
+                                    <th>Favorite</th>
+                                    <th>Language</th>
+                                    <th>Started</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1 (tied)</td>
+                                    <td>C++</td>
+                                    <td>2020</td>
+                                </tr>
+                                <tr>
+                                    <td>1 (tied)</td>
+                                    <td>C#</td>
+                                    <td>2023</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>C</td>
+                                    <td>2019</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Javascript</td>
+                                    <td>2023</td>
+                                </tr>
+                                <tr>
+                                    <td>4</td>
+                                    <td>Python</td>
+                                    <td>2020</td>
+                                </tr>
+                                <tr>
+                                    <td>5</td>
+                                    <td>SQL</td>
+                                    <td>2024</td>
+                                </tr>
+                                <tr>
+                                    <td>6</td>
+                                    <td>Assembly x86/MIPS</td>
+                                    <td>2021</td>
+                                </tr>
+                            </tbody>
+                        </Table>
                     </Col>
                 </Row>
             </Container>
